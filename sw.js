e5261d8ecabce400273e9677345e583409fdb552
vamsikittukrishna-sys <vamsikittu.krishna@gmail.com>
2026-03-19 08:54:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,28 @@
-const CACHE_NAME = 'courtmaster-pro-v10';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'courtmaster-pro-v123';
+const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png'];
 
+// The Magic Cache Buster
 self.addEventListener('install', event => {
+  self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    }).then(() => self.clients.claim()) 
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
