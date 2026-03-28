2ed723b97bff52b00080f65ec99928fdbef5e461
vamsikittukrishna-sys <vamsikittu.krishna@gmail.com>
2026-03-28 19:41:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'courtmaster-pro-v135';
+const CACHE_NAME = 'courtmaster-pro-v136';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png'];
 
 // The Magic Cache Buster
